af543912ce0870aa90d50b3834dd696f850107b5
Emilian Manolache <emi.manolache@gmail.com>
2021-01-29 14:50:51 +0200

    Rename it

--- app.routes.js
+++ app.routes.js
@@ -3,6 +3,6 @@ let router = express.Router();
 
 let Handler = require('./controllers/handler');
 
-router.post('/api/process', Handler.process);
+router.get('/api/process', Handler.process);
 
 module.exports = router;
